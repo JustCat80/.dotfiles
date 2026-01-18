f5c4ae7c4971ac9a3a3f82e427eb96a5186a5c65
Cat <50190590+JustCat80@users.noreply.github.com>
2026-01-18 13:45:25 -0600

    update ls commands

--- HOME/fish/.config/fish/functions/l.fish
+++ HOME/fish/.config/fish/functions/l.fish
@@ -1,3 +1,3 @@
-function l --wraps=ls --description 'alias l=ls -C -p -h --color=auto --group-directories-first'
-   command ls -C -p -h --color=auto --group-directories-first $argv
+function l --wraps=ls --description 'alias l=ls -C -p -h -F --color=auto --group-directories-first'
+   command ls -C -p -h -F --color=auto --group-directories-first $argv
 end
--- HOME/fish/.config/fish/functions/ls.fish
+++ HOME/fish/.config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
-function ls --description 'alias ls=ls -C -p -h -A --color=auto --group-directories-first'
-    command ls -C -p -h -A --color=auto --group-directories-first $argv
+function ls --description 'alias ls=ls -C -p -h -F -A --color=auto --group-directories-first'
+    command ls -C -p -h -F -A --color=auto --group-directories-first $argv
 end
