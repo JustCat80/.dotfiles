c524b289b861a46428a36bc3f41b1eaaf6be1ee9
Cat <50190590+JustCat80@users.noreply.github.com>
2026-01-18 18:46:44 -0600

    improve `l` command, make `ls` call `l -A`

--- HOME/fish/.config/fish/functions/l.fish
+++ HOME/fish/.config/fish/functions/l.fish
@@ -1,3 +1,20 @@
-function l --description 'The same as the ls function but without showing hidden directories'
-    command ls -C -p -h -F --color=auto --group-directories-first $argv
+function l --description 'The same as the ls function but does not show hidden directories if there are more than 10'
+    set -f lsOpts -C -p -h -F --color=auto --group-directories-first
+    
+    argparse -u 'A/almost-all' -- $argv #removes -A from vars
+    
+    set -f dir $argv
+    if not count $argv > /dev/null
+        set -f dir "."
+    end
+    
+    set -f hiddenFileCount $(math $(find $dir -maxdepth 1 -name ".*" | wc -l) -1)
+    if set -ql _flag_A
+      or test $hiddenFileCount -lt 10
+        command ls $lsOpts -A $argv_opts $argv
+        return
+    else
+        echo -e "\e[31m$hiddenFileCount hidden items\e[0m"
+        command ls $lsOpts $argv_opts $argv
+    end
 end
