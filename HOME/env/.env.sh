aca06532ed16f62a1c23b053051021a7f734cd7b
Cat <50190590+JustCat80@users.noreply.github.com>
2026-01-24 19:37:27 -0600

    Update .env.sh

--- HOME/env/.env.sh
+++ HOME/env/.env.sh
@@ -3,3 +3,10 @@ export XDG_DATA_HOME="$HOME/.local/share"
 
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_STATE_HOME="$HOME/.local/state"
+
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME/java"
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+
+export CUDA_CACHE_PATH="$XDG_CACHE_HOME/nv"
+
