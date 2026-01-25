ab9bba1fe18ea3df5225b6b33a8e07b2af8ec786
Cat <50190590+JustCat80@users.noreply.github.com>
2026-01-24 22:36:29 -0600

    Update .env.sh

--- HOME/env/.env.sh
+++ HOME/env/.env.sh
@@ -7,6 +7,10 @@ export XDG_STATE_HOME="$HOME/.local/state"
 export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 
+export HISTFILE="$XDG_STATE_HOME"/bash/history
+export GTK_RC_FILES="$XDG_CONFIG_HOME"/gtk-1.0/gtkrc
+export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc":"$XDG_CONFIG_HOME/gtk-2.0/gtkrc.mine"
+
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME"/npm/npmrc
 export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME/java"
 
